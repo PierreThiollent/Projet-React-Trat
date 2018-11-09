098c0aacb5463e3327848dffdfa989425297301f
KoXo <clement.langendorf@gmail.com>
2018-11-09 12:24:04 +0100

    Profil Vue, ajout du score

--- Trat/Components/Profile.js
+++ Trat/Components/Profile.js
@@ -1,9 +1,64 @@
 import React from 'react';
 import {Dimensions, Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import {Avatar} from "react-native-elements";
+import ProgressCircle from 'react-native-progress-circle'
+
 
 export default class Profile extends React.Component {
+
+    _percentage = () => {
+        this.setState({
+            score: this.state.score + 1,
+        });
+        if (this.state.percent >= 100 ) {
+            this.setState({
+                    percent: 0,
+                    level: this.state.level + 1,
+                }
+            );
+
+        }
+        else{
+            if (this.state.score < 10) {
+                this.setState({
+                        percent: this.state.percent + 10,
+                    }
+                );
+                console.log(this.state.percent);
+            }
+            else if (this.state.score >= 10 && this.state.score < 28 ) {
+                this.setState({
+                    percent: this.state.percent + 6.5,
+                }
+            );
+            }
+            else {
+                this.setState({
+                    score: 0,
+                    percent: 0,
+                })
+            }
+
+        }
+
+        };
+
+
+
+
+
+
+    constructor(props) {
+        super(props);
+        this.state = ({
+            score: 0,
+            level: 0,
+            percent: 0,
+        })
+    }
+
     render() {
+        console.log(this.state.percent);
+        console.log(this.state.score);
         return (
             <ImageBackground source={require('../assets/Images/Profile.png')} style={styles.container}>
                 <View style={styles.burger}>
@@ -17,20 +72,24 @@ export default class Profile extends React.Component {
                 <View style={styles.header}>
                         <Text style={styles.title}>PROFIL</Text>
 
-                </View>
                 <View style={styles.userInfos}>
                     <Text style={styles.title}>Martin HERICHE</Text>
                     <Text style={{fontSize:17, color:'white', marginLeft: 15}}>Etudiant en Web Design</Text>
                 </View>
+                </View>
                 <View style={styles.avatar}>
-                    <Avatar
-                        width={200}
-                        height={200}
-                        rounded
-                        source={require('../assets/Images/profileImage.jpg')}
-                    />
-                    <Text style={styles.avatarText}>Niveau 12</Text>
-                    <Text style={{ color:'white'}}>800 / 1200xp</Text>
+                    <ProgressCircle
+                        percent={this.state.percent}
+                        radius={90}
+                        borderWidth={8}
+                        color="#000"
+                        shadowColor="#FFF"
+                        bgColor="rgba(0, 0, 0, 0.3)"
+                    >
+                        <Image source={require('../assets/Images/profileImage.jpg')} />
+                    </ProgressCircle>
+                    <Text style={styles.avatarText}>Niveau {this.state.level}</Text>
+                    <Text style={styles.avatarText}>{this.state.percent}%</Text>
                 </View>
                 <View style={styles.gamesContainer}>
                     <TouchableOpacity>
@@ -43,7 +102,7 @@ export default class Profile extends React.Component {
                         <Text style={[styles.gamesText, {marginTop: 10}]}>Musique</Text>
                         <Text style={styles.gamesText}>Niveau 5</Text>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={() => this._percentage()}>
                         <Image source={require('../assets/Images/GameFood.png')}/>
                         <Text style={[styles.gamesText, {marginTop: 10}]}>Nourriture</Text>
                         <Text style={styles.gamesText}>Niveau 5</Text>
@@ -59,19 +118,18 @@ const styles = StyleSheet.create({
         flex: 1,
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+        backgroundColor:'black'
     },
     header: {
-        alignItems:'center',
-        justifyContent:'space-between',
+        marginTop: 25,
+        justifyContent:'flex-start',
         width:100+'%',
-        height:100,
-        flexDirection: 'row',
-        marginTop: 10
+        height:50+"%",
     },
     title: {
         marginLeft: 15,
         color: 'white',
-        fontSize: 30
+        fontSize: 25
     },
     burger: {
         position: 'absolute',
@@ -80,12 +138,15 @@ const styles = StyleSheet.create({
         zIndex: 10,
     },
     userInfos: {
-        width:100+'%',
+        marginTop: 20,
+        width:250,
     },
     avatar:{
         alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 60
+        position: 'absolute',
+        bottom: '30%',
+        right: '25%'
+
     },
     avatarText: {
         color:'white',
